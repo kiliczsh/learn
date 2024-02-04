7e47d8617f33609f3d4d518470188226659c2bbd
kiliczsh <muhammeddkilicc@gmail.com>
2024-02-04 20:18:56 +0300

    Update primary color of shadcn/ui to orange

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Badge } from "@/components/ui/badge";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Badge variant="destructive">page.tsx</Badge>
+      <Badge>page.tsx</Badge>
     </main>
   );
 }
