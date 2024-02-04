831bf0107f96135fe69a9a4a68b336b195eb28a8
kiliczsh <muhammeddkilicc@gmail.com>
2024-02-04 22:26:35 +0300

    Create form demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Login from "@/components/demo/login";
 import Faq from "@/components/demo/faq";
+import { Separator } from "@/components/ui/separator";
+import DemoForm from "@/components/demo/demoform";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-auto items-center justify-between p-4 md:p-24">
+      <DemoForm />
+      <span className="pt-10 pb-10" />
       <Login />
       <span className="pt-10 pb-10" />
       <Faq />
